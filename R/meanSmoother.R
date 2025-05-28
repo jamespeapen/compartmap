7a95e6708e33a336a1e72f3f708e3b08b2555d94
James Eapen <james.eapen@vai.org>
2025-05-28 10:13:45 -0400

    fix(meanSmoother): always na.rm, median will fail if NAs present

--- R/meanSmoother.R
+++ R/meanSmoother.R
@@ -6,7 +6,6 @@
 #' @param k         Number of windows to use (default = 1 to smooth with 1
 #'                  window on either side of a position)
 #' @param iters     Number of iterations to smooth (default is 2)
-#' @param na.rm     Whether to remove NAs prior to smoothing (TRUE)
 #' @param delta     Convergence threshhold (overrides iter if > 0; default is 0)
 #' @param weights   Weights, if using any (NULL)
 #'
@@ -21,7 +20,7 @@
 #' smooth.dummy <- meanSmoother(dummy, delta = 1e-3)
 #'
 #' @export
-meanSmoother <- function(mat, k = 1, iters = 2, na.rm = TRUE, delta = 0, weights = NULL) {
+meanSmoother <- function(mat, k = 1, iters = 2, delta = 0, weights = NULL) {
   if (k == 0) {
     message("Returning unsmoothed mat as 'k' = 0")
     return(mat)
@@ -36,8 +35,8 @@ meanSmoother <- function(mat, k = 1, iters = 2, na.rm = TRUE, delta = 0, weights
   while (pos < iters & eps > delta) {
     mat0 <- mat
     pos <- pos + 1
-    mat <- .meanSmoother.internal(mat0, weights = weights, k = k, na.rm = na.rm)
-    eps <- median(abs(mat - mat0))
+    mat <- .meanSmoother.internal(mat0, weights = weights, k = k)
+    eps <- median(abs(mat - mat0), na.rm = TRUE)
   }
 
   mat
@@ -47,7 +46,7 @@ meanSmoother <- function(mat, k = 1, iters = 2, na.rm = TRUE, delta = 0, weights
 # helper fn
 # finds the first and last eligible positions to smooth as well as excess bins
 # beyond eligible positions.
-.meanSmoother.internal <- function(mat, weights, k, na.rm = TRUE) {
+.meanSmoother.internal <- function(mat, weights, k) {
   n <- length(mat)
   y <- rep(NA, n)
 
@@ -57,16 +56,16 @@ meanSmoother <- function(mat, k = 1, iters = 2, na.rm = TRUE, delta = 0, weights
 
   # note that na.rm can create issues
   for (pos in first_pos:last_pos) {
-    y[pos] <- .window.mean(mat, weights, pos, k = k, na.rm = na.rm)
+    y[pos] <- .window.mean(mat, weights, pos, k = k)
   }
 
   # it is possible for windows to be 0 in these loops
   for (pos in 1:k) {
-    y[pos] <- .window.mean(mat, weights, pos, k = pos - 1, na.rm = na.rm)
+    y[pos] <- .window.mean(mat, weights, pos, k = pos - 1)
   }
 
   for (pos in excess_bins) {
-    y[pos] <- .window.mean(mat, weights, pos, k = n - pos, na.rm = na.rm)
+    y[pos] <- .window.mean(mat, weights, pos, k = n - pos)
   }
 
   y
@@ -74,12 +73,12 @@ meanSmoother <- function(mat, k = 1, iters = 2, na.rm = TRUE, delta = 0, weights
 
 
 # helper fn
-.window.mean <- function(mat, weights, pos, k, na.rm = TRUE) {
+.window.mean <- function(mat, weights, pos, k) {
   if (k < 1) {
     return(mat[pos])
   }
   endpos <- pos + k
   startpos <- pos - k - 1
   stride <- seq(startpos, endpos)
-  weighted.mean(mat[stride], weights = weights[stride], na.rm = na.rm)
+  weighted.mean(mat[stride], weights = weights[stride], na.rm = TRUE)
 }

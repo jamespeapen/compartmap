f9bbbf75a2a713b4feed681b81727811e10a62d3
James Eapen <james.eapen@vai.org>
2025-06-02 12:55:07 -0400

    test(getSVD): test .centerMatrix

--- tests/testthat/test-getSVD.R
+++ tests/testthat/test-getSVD.R
@@ -14,3 +14,17 @@ test_that(".getUV", {
     abs(svd$v)
   )
 })
+
+test_that(".centerMatrix", {
+  m <- matrix(rnorm(100), ncol = 10)
+
+  expected <- t(apply(m, 1, function(i) {
+    i - mean(i)
+  }))
+
+  expect_equal(
+    compartmap:::.centerMatrix(m),
+    expected,
+    check.attributes = FALSE
+  )
+})

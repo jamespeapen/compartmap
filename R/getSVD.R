cfe5bedb7e5dc8f0e32f24179ec0c3bff205ff31
James Eapen <james.eapen@vai.org>
2025-06-02 12:54:52 -0400

    refactor(getSVD): extract matrix centering function

--- R/getSVD.R
+++ R/getSVD.R
@@ -16,14 +16,12 @@
 #'
 #' dummy <- matrix(rnorm(10000), ncol=25)
 #' sing_vec <- getSVD(dummy, k = 1, sing.vec = "right")
-getSVD <- function (matrix, k = 1, sing.vec = c("left", "right")) {
+getSVD <- function(matrix, k = 1, sing.vec = c("left", "right")) {
   sing.vec <- match.arg(sing.vec)
 
-  #center the matrix
-  matrix <- t(scale(t(matrix), center = TRUE, scale = FALSE))
-
+  matrix <- .centerMatrix(matrix)
   p.mat <- .getUV(matrix, k, sing.vec)
-  csums <- colSums(matrix, na.rm=TRUE)
+  csums <- colSums(matrix, na.rm = TRUE)
 
   # check for negative correlation
   # flip sign as necessary to ensure signal is associated with pos. values
@@ -32,6 +30,12 @@ getSVD <- function (matrix, k = 1, sing.vec = c("left", "right")) {
   }
 
   p.mat * sqrt(length(p.mat)) # Chromosome length normalization
+}
+
+# Centre the matrix subtracting the row means from each row
+#' @keywords internal
+.centerMatrix <- function(mat) {
+  t(scale(t(mat), center = TRUE, scale = FALSE))
 }
 
 #' Run SVD and get left or right singular vectors
@@ -44,9 +48,6 @@ getSVD <- function (matrix, k = 1, sing.vec = c("left", "right")) {
 #'
 #' @keywords internal
 .getUV <- function(matrix, k, sing.vec) {
-  SVD <- runSVD(matrix, k=k, BSPARAM=IrlbaParam())
-  switch(sing.vec,
-    left = SVD$u,
-    right = SVD$v
-  )
+  SVD <- runSVD(matrix, k = k, BSPARAM = IrlbaParam())
+  switch(sing.vec, left = SVD$u, right = SVD$v)
 }

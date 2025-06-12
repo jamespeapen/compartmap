729d5a0ed46a335017085dca3431c08e08862cd7
James Eapen <james.eapen@vai.org>
2025-06-12 11:07:56 -0400

    test(test-getABSignal): move .isCompartmentOpen test to test-getABSignal

---
+++ tests/testthat/test-getABSignal.R
@@ -0,0 +1,14 @@
+# .isCompartmentOpen
+test_that(".isCompartmentOpen", {
+  .isCompartmentOpen <- compartmap:::.isCompartmentOpen
+  expect_true(
+    .isCompartmentOpen(is.atac_or_rna = TRUE, eigen = 1, cutoff = 0),
+    .isCompartmentOpen(is.atac_or_rna = FALSE, eigen = 0, cutoff = 0),
+    .isCompartmentOpen(is.atac_or_rna = FALSE, eigen = -1, cutoff = 0)
+  )
+  expect_false(
+    .isCompartmentOpen(is.atac_or_rna = TRUE, eigen = 0, cutoff = 0),
+    .isCompartmentOpen(is.atac_or_rna = TRUE, eigen = -1, cutoff = 0),
+    .isCompartmentOpen(is.atac_or_rna = FALSE, eigen = 1, cutoff = 0)
+  )
+})

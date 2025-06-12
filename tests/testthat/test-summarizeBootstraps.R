00462fd4cccecdb42ca6bc72bd159847caa31ed3
James Eapen <james.eapen@vai.org>
2025-06-12 11:39:40 -0400

    test(summarizeBoostraps): .getCI

--- tests/testthat/test-summarizeBootstraps.R
+++ tests/testthat/test-summarizeBootstraps.R
@@ -1,3 +1,33 @@
-# summarizeBoostraps
-test_that("summarizeBoostraps", {
+
+test_that(".getCI", {
+  gr <- GRanges(c(
+    "chr1:1-10",
+    "chr1:10-20",
+    "chr1:21-30",
+    "chr1:31-40",
+    "chr1:41-50",
+    "chr1:51-60"
+  ))
+
+  est.ab <- data.frame(
+    compartments = rep(c("open", "closed"), 3),
+    boot.open = c(100, 50, 5, 2, 1, 0),
+    boot.closed = c(0, 50, 95, 98, 99, 100)
+  )
+  mcols(gr) <- est.ab
+
+  gr.expected <- gr
+  successes <- c(100, 50, 5, 98, 1, 100)
+  failures <- c(0, 50, 95, 2, 99, 0)
+  expectedCI <- agrestiCoullCI(successes, failures, 0.95)
+  mcols(gr.expected) <- cbind(
+    mcols(gr),
+    agrestiCoullCI(successes, failures, 0.95)
+  )
+
+  expect_equal(gr.expected, compartmap:::.getCI(gr, 0.95))
+})
+
+
+test_that("summarizeBootstraps", {
 })

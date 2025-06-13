1d6f54b16cee2efbe29484316ce2dcbc68895220
James Eapen <james.eapen@vai.org>
2025-06-13 11:49:36 -0400

    test(summarizeBootstraps): summarizeBootstraps

--- tests/testthat/test-summarizeBootstraps.R
+++ tests/testthat/test-summarizeBootstraps.R
@@ -1,4 +1,3 @@
-
 test_that(".getCI", {
   gr <- GRanges(c(
     "chr1:1-10",
@@ -41,4 +40,76 @@ test_that(".getSummary", {
 })
 
 test_that("summarizeBootstraps", {
+  # create the non-bootstrapped calls as all open
+  est.ab <- GRanges(c(
+    "chr1:1-10",
+    "chr1:11-20",
+    "chr1:21-30",
+    "chr1:31-40",
+    "chr1:41-50",
+    "chr1:51-60"
+  ))
+  mcols(est.ab) <- data.frame(pc = rep(1, 6))
+  est.ab$compartments <- ifelse(mcols(est.ab)$pc > 0, "open", "closed")
+  gr1 <- est.ab
+  gr2 <- est.ab
+  mcols(gr2[3, ]) <- NA
+
+  removeEmptyBoots(list(gr1, gr2))
+
+  expect_message(summarizeBootstraps(list(gr1, gr1), gr1), "Summarizing bootstraps")
+
+  tester <- function(one, two, expected) {
+    mcols(gr1) <- data.frame(pc = one)
+    gr1$compartments <- ifelse(mcols(gr1)$pc > 0, "open", "closed")
+
+    mcols(gr2) <- data.frame(pc = two)
+    gr2$compartments <- ifelse(mcols(gr2)$pc > 0, "open", "closed")
+
+    boot.list <- list(gr1, gr2)
+    expect_equal(
+      mcols(summarizeBootstraps(boot.list, est.ab))[, 6:8],
+      expected
+    )
+  }
+
+  # set up the successes and failures based on the all open est.ab
+  test_set <- list(
+    # equal successes and failures
+    list(one = rep(-1, 6), two = rep(1, 6), ci = agrestiCoullCI(1, 1, 0.95)),
+    # only successes
+    list(one = rep(1, 6), two = rep(1, 6), ci = agrestiCoullCI(2, 0, 0.95)),
+    # only failures
+    list(one = rep(0, 6), two = rep(0, 6), ci = agrestiCoullCI(0, 2, 0.95))
+  )
+
+  lapply(test_set, function(set) {
+    ci <- set$ci
+    expected <- rbind(ci, ci, ci, ci, ci, ci) |> DataFrame()
+    tester(set$one, set$two, expected)
+  })
+
+  # alternating success/failures on one
+  mcols(gr1) <- data.frame(pc = rep(c(-1, 1), 3))
+  gr1$compartments <- ifelse(mcols(gr1)$pc > 0, "open", "closed")
+
+  mcols(gr2) <- data.frame(pc = rep(0, 6))
+  gr2$compartments <- ifelse(mcols(gr2)$pc > 0, "open", "closed")
+
+  boot.list <- list(gr1, gr2)
+
+  expected <- rbind(
+    agrestiCoullCI(0, 2, 0.95),
+    agrestiCoullCI(1, 1, 0.95),
+    agrestiCoullCI(0, 2, 0.95),
+    agrestiCoullCI(1, 1, 0.95),
+    agrestiCoullCI(0, 2, 0.95),
+    agrestiCoullCI(1, 1, 0.95)
+  ) |>
+    DataFrame()
+
+  expect_equal(
+    mcols(summarizeBootstraps(boot.list, est.ab))[, 6:8],
+    expected
+  )
 })

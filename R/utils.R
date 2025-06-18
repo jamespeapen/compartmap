d254141d974b0a169da0ddc0c1301331dcb4a3fc
James Eapen <james.eapen@vai.org>
2025-06-18 12:57:54 -0400

    docs(utils): add info on 8000/4kb resize done for getOpenSeas
    removes rtracklayer import

--- R/utils.R
+++ R/utils.R
@@ -430,10 +430,12 @@ filterOpenSea <- function(
 
 #' Gather open sea CpG from a GRanges of CpG islands
 #'
-#' This function accepts a GRanges input of CpG islands that can
-#' be derived from UCSC table browser and rtracklayer::import(yourbed.bed). It
-#' resizes the inteval to create 4kb flanking open sea regions around the CpG
-#' islands as defined by [Fortin and Hansen (Genome Biology, 2015)](https://doi.org/10.1186/s13059-015-0741-y).
+#' This function accepts a GRanges input of CpG islands that can be derived
+#' from UCSC table browser and rtracklayer::import(yourbed.bed). It resizes the
+#' intevals to create 4kb flanking regions around CpG islands. Open sea
+#' regions, as defined by [Fortin by and Hansen (Genome Biology,
+#' 2015)](https://doi.org/10.1186/s13059-015-0741-y), are outside this flanking
+#' regions and obtained as their complement.
 #'
 #' @param gr Input GRanges of CpG islands
 #'

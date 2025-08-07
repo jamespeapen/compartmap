f157cd2e37d42c04340b7687d8d7026cab650129
James Eapen <james.eapen@vai.org>
2025-08-07 13:23:50 -0400

    test(transformTFIDF): remove test for binarizeMatrix

--- tests/testthat/test-transformTFIDF.R
+++ tests/testthat/test-transformTFIDF.R
@@ -8,18 +8,6 @@ test_that("transformTFIDF", {
 })
 # }}}
 
-# .binarizeMatrix {{{
-test_that(".binarizeMatrix", {
-  mat <- c(-20, -5, -1, -0.5, 0, 1, 0.25, 2, 10, 15) |> matrix(ncol = 5)
-  mat.binary <- c(0, 0, 0, 0, 0, 1, 1, 1, 1, 1) |> matrix(ncol = 5)
-  expect_equal(
-    compartmap:::.binarizeMatrix(mat),
-    mat.binary
-  )
-  # TODO: handling NA, Inf
-})
-# }}}
-
 # .tfidf {{{
 test_that(".tfidf", {
   i <- c(1, 3:8)

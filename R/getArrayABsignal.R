62e6e41bd532fdde099305799e5613eb8917ca24
James Eapen <james.eapen@vai.org>
2025-10-23 12:48:37 -0400

    !fix(sc|arrayCompartments): add missing args and reorder for consistency

--- R/getArrayABsignal.R
+++ R/getArrayABsignal.R
@@ -5,19 +5,19 @@
 #'
 #' @param obj Input SummarizedExperiment object
 #' @param res Compartment resolution in bp
-#' @param parallel Whether to run samples in parallel
 #' @param chr What chromosome to work on (leave as NULL to run on all chromosomes)
 #' @param targets Samples/cells to shrink towards
 #' @param preprocess Whether to preprocess the arrays prior to compartment inference
+#' @param parallel Whether to run samples in parallel
 #' @param cores How many cores to use when running samples in parallel
 #' @param bootstrap Whether we should perform bootstrapping of inferred compartments
 #' @param num.bootstraps How many bootstraps to run
-#' @param genome What genome to work on ("hg19", "hg38", "mm9", "mm10")
-#' @param other Another arbitrary genome to compute compartments on
-#' @param array.type What type of array is this ("hm450", "EPIC")
-#' @param group Whether to treat this as a group set of samples
 #' @param boot.parallel Whether to run the bootstrapping in parallel
 #' @param boot.cores How many cores to use for the bootstrapping
+#' @param genome What genome to work on ("hg19", "hg38", "mm9", "mm10")
+#' @param other Another arbitrary genome to compute compartments on
+#' @param group Whether to treat this as a group set of samples
+#' @param array.type What type of array is this ("hm450", "EPIC")
 #'
 #' @return A RaggedExperiment of inferred compartments
 #' @import SummarizedExperiment
@@ -33,8 +33,8 @@
 #'   data("array_data_chr14", package = "compartmap")
 #'   array_compartments <- getArrayABsignal(
 #'     array.data.chr14,
-#'     parallel=FALSE,
 #'     chr="chr14",
+#'     parallel=FALSE,
 #'     bootstrap=FALSE,
 #'     genome="hg19",
 #'     array.type="hm450"
@@ -43,19 +43,19 @@
 getArrayABsignal <- function(
   obj,
   res = 1e6,
-  parallel = TRUE,
   chr = NULL,
   targets = NULL,
   preprocess = TRUE,
+  parallel = TRUE,
   cores = 2,
   bootstrap = TRUE,
   num.bootstraps = 1000,
-  genome = c("hg19", "hg38", "mm9", "mm10"),
-  other = NULL,
-  array.type = c("hm450", "EPIC"),
-  group = FALSE,
   boot.parallel = TRUE,
-  boot.cores = 2
+  boot.cores = 2,
+  genome = c("hg19", "hg38", "mm9", "mm10"),
+  group = FALSE,
+  other = NULL,
+  array.type = c("hm450", "EPIC")
 ) {
   verifySE(obj)
   verifyCoords(obj)
--- R/scCompartments.R
+++ R/scCompartments.R
@@ -5,12 +5,14 @@
 #'
 #' @param obj Input SummarizedExperiment object
 #' @param res Compartment resolution in bp
-#' @param parallel Whether to run samples in parallel
 #' @param chr What chromosome to work on (leave as NULL to run on all chromosomes)
 #' @param targets Samples/cells to shrink towards
+#' @param parallel Whether to run samples in parallel
 #' @param cores How many cores to use when running samples in parallel
 #' @param bootstrap Whether we should perform bootstrapping of inferred compartments
 #' @param num.bootstraps How many bootstraps to run
+#' @param boot.parallel Whether to run the bootstrapping in parallel
+#' @param boot.cores How many cores to use for the bootstrapping
 #' @param genome What genome to work on ("hg19", "hg38", "mm9", "mm10")
 #' @param group Whether to treat this as a group set of samples
 #' @param assay What type of single-cell assay is the input data ("atac" or "rna")
@@ -24,20 +26,22 @@
 #' data("k562_scrna_chr14", package = "compartmap")
 #' sc_compartments <- scCompartments(
 #'   k562_scrna_chr14,
-#'   parallel = FALSE,
 #'   chr = "chr14",
+#'   parallel = FALSE,
 #'   bootstrap = FALSE,
 #'   genome = "hg19"
 #' )
 scCompartments <- function(
   obj,
   res = 1e6,
-  parallel = FALSE,
   chr = NULL,
   targets = NULL,
+  parallel = FALSE,
   cores = 2,
   bootstrap = TRUE,
   num.bootstraps = 100,
+  boot.parallel = FALSE,
+  boot.cores = 2,
   genome = c("hg19", "hg38", "mm9", "mm10"),
   group = FALSE,
   assay = c("atac", "rna")
@@ -51,11 +55,11 @@ scCompartments <- function(
   verifyAssayNames(obj, assay = assay)
   getCompartments(
     obj = obj,
-    assay = "atac",
+    assay = assay,
     res = res,
-    parallel = parallel,
     chr = chr,
     targets = targets,
+    parallel = parallel,
     cores = cores,
     bootstrap = bootstrap,
     num.bootstraps = num.bootstraps,

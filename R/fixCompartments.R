a0fd2d790f2453396b1835e53713184e73c7a220
James Eapen <james.eapen@vai.org>
2025-11-26 16:37:06 -0500

    fix(fixCompartments): create generic for fixing GRanges vs RaggedExperiment
    So that the functions are pure for Bioc requirements and so if a
    RaggedExperiment is provided, its also returned for S7 compatibility -
    make scCompartmapCall from both fixed and non-fixed `scCompartments`
    results.

--- R/fixCompartments.R
+++ R/fixCompartments.R
@@ -2,7 +2,7 @@
 #'
 #' @name fixCompartments
 #'
-#' @param obj Input RaggedExperiment or output of condenseSE
+#' @param x Input RaggedExperiment or output of condenseSE
 #' @param min.conf Minimum confidence score to use
 #' @param parallel Whether to run in parallel
 #' @param cores How many cores to use if running in parallel
@@ -10,24 +10,28 @@
 #' @return A "fixed" set of compartments
 #' @import RaggedExperiment
 #' @import SummarizedExperiment
+#' @importFrom methods setGeneric setMethod standardGeneric
 #' @export
 #'
 #' @examples
-fixCompartments <- function(obj, min.conf = 0.8, parallel = FALSE, cores = 1) {
-  # this function will invert or "fix" compartments based on bootstrapping results
-  if (is(obj, "RaggedExperiment")) obj <- condenseSE(obj, sample.name = colnames(assay(obj)))
+setGeneric("fixCompartments", function(x, min.conf = 0.8, parallel = FALSE, cores = 1) {
+  standardGeneric("fixCompartments")
+})
 
-  # if we somehow only have 1 sample
-  if (is(obj, "GRanges")) {
-    return(flipper(obj, min.conf))
-  }
+setMethod("fixCompartments", "GRanges", function(x, min.conf = 0.8, parallel = FALSE, cores = 1) {
+  message("Assuming we only have a single sample to process")
+  message("Fixing compartments using a minimum confidence score of ", min.conf * 100, "%")
+  flipper(x, min.conf)
+})
 
+setMethod("fixCompartments", "RaggedExperiment", function(x, min.conf = 0.8, parallel = FALSE, cores = 1) {
+  obj <- condenseSE(x, sample.name = colnames(assay(x)))
   message("Fixing compartments using a minimum confidence score of ", min.conf * 100, "%")
   # go through and invert compartments based on the min.conf
   flip_compartments_lst <- mclapply(obj, flipper, min.conf, mc.cores = ifelse(parallel, cores, 1))
   names(flip_compartments_lst) <- names(obj)
-  return(flip_compartments_lst)
-}
+  RaggedExperiment(flip_compartments_lst)
+})
 
 #' Helper to invert, or "fix", compartments that have a minimum confidence score (1-min.conf)
 #'
@@ -41,8 +45,6 @@ flipper <- function(input_obj, min.conf) {
     stop("Bootstrapping was not performed. Cannot fix compartments.")
   }
 
-  message("Assuming we only have a single sample to process.")
-  message("Fixing compartments using a minimum confidence score of ", min.conf * 100, "%")
   invert_compartments <- apply(mcols(input_obj), 1, .inverter, min.conf)
   mcols(input_obj)$flip.compartment <- invert_compartments
 

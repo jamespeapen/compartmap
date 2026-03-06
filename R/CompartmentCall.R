ed1225fdf2165c1f3cc5a3abe6459f43aa42f5f2
James Eapen <james.eapen@vai.org>
2026-03-06 10:23:31 -0500

    refactor(S7): separate main S7 objects, add folding for getters, setters

---
+++ R/CompartmentCall.R
@@ -0,0 +1,532 @@
+#' Compartment Call objects for analysis (experimental)
+#'
+#' These S7 classes hold compartment call values, and their genomic region and
+#' resolution metadata as well as methods to aid analysis and visualization.
+#' `CompartmentCall()` is primarily a parent class from which
+#' `CompartmapCall()` and `MultiCompartmapCall()` are derived.
+#'
+#' @details
+#'
+#' ## Objects
+#'
+#' `CompartmentCall` is constucted from a vector of the call values, a
+#' `GenomicRanges::GRanges()` object of the bins, and the resolution of the
+#' bins. Given a `GRanges` of bin coordinates, it can be used to store singular
+#' values/eigenvectors from Hi-C compartment analysis. Along with
+#' `CompartmapCall` objects, they can be combined in a `MultiCompartmapCall` to
+#' compare against each other. All `CompartmentCall` methods work on every
+#' other *compartmap* S7 object.
+#'
+#' `CompartmapCall()` is constructed from the `GRanges` output of
+#' `scCompartments(group = TRUE)`. `MultiCompartmapCall()` holds multiple
+#' `CompartmapCall()` objects at the same resolution, and is constructed with a
+#' list of `CompartmentCall` or `CompartmapCall` objects.
+#'
+#' For single-cell level compartment inferences use `scCompartmapCall()`, which
+#' inherits from `MultiCompartmapCall`. This class holds multiple single-cell
+#' level compartment calls, taking the `RaggedExperiment::RaggedExperiment()`
+#' from `scCompartments()` as its constructor. Since it inherits from
+#' `MultiCompartmapCall`, all `MultiCompartmapCall` methods work on
+#' `scCompartmapCall` objects.
+#'
+#' All objects may be subset to required indices with `object[row indices,
+#' column indices]`. Their dimensions can be accessed with `dim()`, `nrow()`
+#' and `ncol()`.
+#'
+#' ## Properties and accessors
+#'
+#' These properties may be accessed with `@` like S4 slots, or using their
+#' accessor functions. Some functions share methods with other Bioconductor
+#' classes like `GRanges` and `SummarizedExperiment`.
+#'
+#' - `name`, `get_name()`: The name of the object, used to identify it, useful
+#' when making `MultiCompartmapCall` objects
+#' - `gr`, `granges()`: A `GRanges` object of the compartment bins
+#' - `df`, `DF()`: a `data.table` of bin indices in column `n` and compartment
+#' call singular values in column `pc`. For `MultiCompartmapCall` and
+#' `scCompartmapCall` objects, this is in a tidy format, with an additional
+#' `name` column.
+#' - `res`, `resolution()`: The genomic resolution at which the compartments
+#' were called
+#' - `unitarized`, `is_unitarized()`: Whether the singular values have been
+#' unitarized
+#' - `seqinfo`, `seqinfo()`: Seqinfo for the object's `GRanges`
+#' - `mat`, `mat()`: A matrix of genomic bins by singular values - a wide
+#' matrix format of the `df` slot. This is used to calculate correlation and
+#' agreement between cells in a `scCompartmapCall` and groups in a
+#' `MultiCompartmapCall` object.
+#'
+#' @param pc The singular values from a compartment call
+#' @param res The binning resolution used
+#' @param gr The GRanges of the bins or the output of `scCompartments` or
+#' `getArrayCompartments` containing the 'pc' column
+#' @param name An identifier for the object. For `CompartmentCall` and
+#' `CompartmapCall`, this is becomes the column name for the object when added
+#' to a `MultiCompartmapCall` object
+#' @param unitarized Whether the singular values have been unitarized
+#'
+#' @import S7
+#' @importFrom data.table data.table .I .N :=
+#' @keywords CompartmentCall
+#' @export
+CompartmentCall <- new_class(
+  "CompartmentCall",
+  properties = list(
+    name = class_character,
+    gr = methods::getClass("GRanges"),
+    df = methods::getClass("data.table"),
+    res = class_numeric,
+    unitarized = class_logical,
+    filtered = class_logical,
+    filter_threshold = class_numeric,
+    seqinfo = methods::getClass("Seqinfo")
+  ),
+  constructor = function(pc, res, gr, name = NULL, unitarized = FALSE) {
+    df <- data.table(pc = as.vector(pc))[, .(n = .I, pc, name = name)]
+    new_object(
+      S7_object(),
+      name = name %||% shQuote(substitute(gr), "cmd2"),
+      gr = granges(gr),
+      df = df,
+      res = res,
+      unitarized = unitarized,
+      filtered = FALSE,
+      filter_threshold = 0,
+      seqinfo = methods::selectMethod('seqinfo', "GRanges")(gr)
+    )
+  },
+  validator = function(self) {
+    if (is.na(genome(self@gr))) {
+      "`gr`'s `genome` must be specified. Set with `genome(gr)` <- [genome]"
+    }
+  }
+)
+S4_register(CompartmentCall)
+
+# GETTERS ==================================================================={{{
+
+#' Get the `@df` slot from a CompartmentCall object.
+#'
+#' `n`: bin indices corresponding to indices of the `GRanges` object in `@gr`
+#' `pc`: compartment call singular values
+#' `name`: The name of the individual `CompartmentCall` in a
+#' `MultiCompartmapCall` object
+#'
+#' @param x A `CompartmentCall` object
+#'
+#' @concept s7getters
+#' @export
+DF <- new_generic("DF", "x", function(x) {
+  S7_dispatch()
+})
+method(DF, CompartmentCall) <- function(x) {
+  x@df[]
+}
+
+#' @concept s7getters
+`nrow.compartmap::CompartmentCall` <- function(x) {
+  length(x@gr)
+}
+
+#' @concept s7getters
+`ncol.compartmap::CompartmentCall` <- function(x) {
+  1
+}
+
+#' @concept s7getters
+#' Get GRanges of the `CompartmentCall`
+#'
+#' @param x A `CompartmentCall` object
+#'
+#' @export
+method(granges, CompartmentCall) <- function(x) {
+  x@gr
+}
+
+#' Get Seqinfo of the `CompartmentCall`
+#'
+#' @param x A `CompartmentCall` object
+#'
+#' @concept s7getters
+#' @export
+method(seqinfo, CompartmentCall) <- function(x) {
+  x@seqinfo
+}
+
+
+#' Get 'seqlevels' of the `CompartmentCall`
+#'
+#' @param x A `CompartmentCall` object
+#'
+#' @concept s7getters
+#' @export
+method(seqlevels, CompartmentCall) <- function(x) {
+  methods::selectMethod('seqlevels', 'GRanges')(x)
+}
+
+#' Get the resolution of the `CompartmentCall`
+#'
+#' @param x A `CompartmentCall` object
+#'
+#' @concept s7getters
+#' @export
+resolution <- new_generic("resolution", "x", function(x) {
+  S7_dispatch()
+})
+method(resolution, CompartmentCall) <- function(x) {
+  x@res
+}
+
+#' Check if the `CompartmentCall` was unitarized
+#'
+#' @param x A `CompartmentCall` object
+#'
+#' @concept s7getters
+#' @export
+is_unitarized <- new_generic("is_unitarized", "x", function(x) {
+  S7_dispatch()
+})
+method(is_unitarized, CompartmentCall) <- function(x) {
+  x@unitarized
+}
+
+#' Get the name of the `CompartmentCall` object
+#'
+#' @param x A `CompartmentCall` object
+#'
+#' @concept s7getters
+#' @export
+get_name <- new_generic("get_name", "x", function(x) {
+  S7_dispatch()
+})
+method(get_name, CompartmentCall) <- function(x) {
+  x@name
+}
+
+#' Check if the `CompartmentCall` was filtered.
+#'
+#' @param x A `CompartmentCall` object
+#'
+#' @concept s7getters
+#' @export
+is_filtered <- new_generic("is_filtered", "x", function(x) {
+  S7_dispatch()
+})
+method(is_filtered, CompartmentCall) <- function(x) {
+  x@filtered
+}
+
+#' Check the threshold at which an object's calls were filtered, returning 0 if not filtered
+#'
+#' @param x A `CompartmentCall` object
+#'
+#' @concept s7getters
+#' @export
+get_filter_threshold <- new_generic("filter_threshold", "x", function(x) {
+  S7_dispatch()
+})
+method(get_filter_threshold, CompartmentCall) <- function(x) {
+  x@filter_threshold
+}
+
+# }}}
+
+# SUBSETTERS ==============================================================={{{
+
+#' Subset rows of a `CompartmentCall` object
+#'
+#' @param x A `CompartmentCall` object
+#' @param i Row indices to subset
+#'
+#' @concept s7ranges
+#' @export
+`[.compartmap::CompartmentCall` <- function(x, i = NULL) {
+  i <- i %||% seq_len(nrow(x@mat))
+  x@df <- x@df[i]
+  x@df[, n := .I][]
+  x@gr <- x@gr[i]
+  seqlevels(x@gr) <- seqlevelsInUse(x@gr)
+  x
+}
+
+#' Subset the `CompartmentCall` object by chromosome
+#'
+#' @param x A `CompartmentCall` object
+#' @param chr A string vector of chromosomes to subset to
+#'
+#' @concept s7ranges
+#' @export
+subset_chr <- new_generic("subset_chr", "x", function(x, chr) {
+  S7_dispatch()
+})
+method(subset_chr, CompartmentCall) <- function(x, chr) {
+  ind <- as.vector(seqnames(x@gr) %gin% chr)
+  x[which(ind)]
+}
+
+# }}}
+
+#' Find overlaps between `CompartmentCall` objects
+#'
+#' @param subject A `CompartmentCall` object
+#' @param query A string vector of chromosomes to subset to
+#'
+#' @concept s7ranges
+#' @export
+method(findOverlaps, list(CompartmentCall, CompartmentCall)) <- function(
+  query,
+  subject,
+  maxgap = -1L,
+  minoverlap = 0L,
+  type = c("any", "start", "end", "within", "equal"),
+  select = c("all", "first", "last", "arbitrary"),
+  ignore.strand = FALSE
+) {
+  query <- query@gr
+  subject <- subject@gr
+  methods::callGeneric()
+}
+
+#' Unitarize the singular values in a `CompartmentCall` or `MultiCompartmapCall`
+#'
+#' @param x A `CompartmentCall` object
+#' @param medianCenter Whether to center the singular values on their median
+#'
+#' @concept s7analysis
+#' @export
+unitarize <- new_generic("unitarize", "x", function(x, medianCenter = TRUE) {
+  S7_dispatch()
+})
+method(unitarize, CompartmentCall) <- function(x, medianCenter = TRUE) {
+  if (is_unitarized(x)) {
+    message(get_name(x), " is already unitarized")
+    return(x)
+  }
+
+  df <- x@df
+  if (inherits(x, "compartmap::MultiCompartmapCall")) {
+    x@mat <- apply(x@mat, 2, .unitarize, medianCenter = medianCenter)
+    x@df <- x@df[, .(n, pc = .unitarize(pc, medianCenter = medianCenter)), by = name][, .(n, pc, name)]
+  } else {
+    x@df <- x@df[, .(n, pc = .unitarize(pc, medianCenter = medianCenter), name)]
+  }
+
+  x@unitarized <- TRUE
+  x
+}
+
+#' Flip the singular values signs in a `CompartmentCall`
+#'
+#' @param x A `CompartmentCall` object
+#'
+#' @concept s7analysis
+#' @export
+flip <- new_generic("flip", "x", function(x) {
+  S7_dispatch()
+})
+method(flip, CompartmentCall) <- function(x) {
+  x@df <- x@df[, .(n, pc = -pc, name)]
+  x
+}
+
+#' Correct the sign of the compartment call vector
+#'
+#' @param x A `CompartmentCall` object
+#'
+#' @concept s7analysis
+#' @export
+fix_sign <- new_generic("fix_sign", "x", function(x) {
+  S7_dispatch()
+})
+method(fix_sign, CompartmentCall) <- function(x) {
+  gr <- x@gr
+  gr$pc <- x@df[, pc]
+  if (flipSign(gr, genome(gr))) {
+    x <- flip(x)
+  }
+  x
+}
+
+#' Fill missing genomic bins in `CompartmentCalls` using a reference GRanges
+#'
+#' Compartmap may drop genomic bins with insufficient data and the resulting
+#' GRanges object may not have all the bins of the region it was run which
+#' means using the same region and resolution on different inputs does not
+#' guarantee the same output bins. Having different set of bins between calls
+#' despite the same input regions and resolution prevents the creation of
+#' `MultiCompartmapCall` objcets that expect the same GRanges across all input
+#' `CompartmentCall` objects. `fill_missing()` adds the missing bins according
+#' to a larger reference set of bins, filling missing data with NA. All
+#' bins in `x` must be present in the reference bins.
+#'
+#' @param x A `CompartmentCall` object
+#' @param ref.gr The `GRanges` object to use as the full reference set of
+#' regions
+#'
+#' @concept s7ranges
+#' @export
+fill_missing <- new_generic("flip", "x", function(x, ref.gr) {
+  S7_dispatch()
+})
+method(fill_missing, CompartmentCall) <- function(x, ref.gr) {
+  ref_length <- length(ref.gr)
+  stopifnot("Reference GRanges is not bigger than CompartmentCall object" = ref_length > length(x@gr))
+  stopifnot("All CompartmentCall bins must be present in the reference GRanges" = all(x@gr %gin% ref.gr))
+
+  ref_idx <- seq_len(ref_length)
+  df <- data.table(n = ifelse(ref.gr %gin% x@gr, ref_idx, NA))
+  df[!is.na(n), `:=`(pc = x@df$pc, name = x@name)]
+  df[, n := .I][]
+  x@gr <- ref.gr
+  x@df <- df
+  x
+}
+
+#' Filter to bins with call values greater than or equal to a threshold value
+#'
+#' @param x A `CompartmentCall` object
+#' @param threshold The absolute value to use for filtering. Rows where any
+#' value is less than this threshold are dropped
+#'
+#' @concept s7analysis
+#' @export
+filter <- new_generic("filter", "x", function(x, threshold = 0.02) {
+  S7_dispatch()
+})
+method(filter, CompartmentCall) <- function(x, threshold = 0.02) {
+  filter_rows <- x@df[, abs(pc) >= threshold]
+  x <- x[filter_rows]
+  x@filtered <- TRUE
+  x@filter_threshold <- threshold
+  x
+}
+
+#' Get the difference between two CompartmentCall objects call values
+#' @param x, y CompartmentCall objects to compare
+#' @concept s7analysis
+#' @rdname CompartmentCall_difference
+#' @export
+`-.compartmap::CompartmentCall` <- function(x, y) {
+  stopifnot("Both objects must have the same GRanges" = length(x@gr) == length(y@gr))
+  stopifnot("Both objects must have the same @df" = nrow(x@df) == nrow(y@df))
+  df <- x@df
+  pc1 <- DF(x)[, pc]
+  pc2 <- DF(y)[, pc]
+  x@df <- x@df[, .(n, pc = pc1 - pc2, name = paste(x@name, "-", y@name))]
+  x
+}
+
+#' Plot singular values of a `CompartmentCall` object
+#'
+#' @param x `CompartmentCall` or `CompartmapCall` object
+#' @param ... Placeholder for the `plot` generic - arguments have not effect
+#' @param type Whether to plot the singular values as `"line"` or `"bar"` plots.
+#' @param label_coords Label the x-axis with genomic coordinates. Uses a
+#' numeric index when set to `FALSE`. Using coordinate labels can severely
+#' crowd the x-axis, especially with Kb-resolution calls.
+#' @param res The resolution to round the genomic coordinates to (kilobase:
+#' "kb" or megabase: "mb")
+#' @param width The width of the `geom_line` if `type = "line"` or the width
+#' of the bar if `type = "bar"` in the plot
+#' @param ylim Upper and lower bound for the y-axis
+#'
+#' @importFrom ggplot2 ggplot geom_line geom_bar scale_y_continuous theme element_text
+#' @concept plotting
+#' @export
+`plot.compartmap::CompartmentCall` <- function(
+  x,
+  ...,
+  type = "line",
+  label_coords = FALSE,
+  res = "kb",
+  width = 0.5,
+  ylim = c(-1, 1)
+) {
+  . <- NULL
+  pc <- NULL
+  if (label_coords) {
+    coord_pd <- x@df[, .(n, pc, coord = grscale(x@gr, res))]
+    p <- ggplot(coord_pd, aes(x = coord, y = pc, group = 1)) +
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+  } else {
+    p <- ggplot(x@df, aes(x = n, y = pc))
+  }
+  p <- p + scale_y_continuous(limits = ylim)
+
+  switch(
+    type,
+    line = p + geom_line(linewidth = width),
+    bar = p + geom_bar(stat = "identity", width = width)
+  )
+}
+
+grscale <- function(gr, res) {
+  scale_factor <- switch(tolower(res), kb = list(1e5, "Kb"), mb = list(1e6, "Mb"))
+  start_scaled <- start(gr) / as.numeric(scale_factor[1])
+  end_scaled <- round(end(gr) / as.numeric(scale_factor[1], 4))
+  paste0(seqlevels(gr), ":", start_scaled, "-", end_scaled, " ", scale_factor[2])
+}
+
+.resolution <- function(res) {
+  fct <- res / 1e5
+  if ((any(fct < 1) | any(fct >= 100))) {
+    stop("Unsupported resolution")
+  }
+  ifelse(fct < 10, paste(fct, "Kb"), paste(fct / 10, "Mb"))
+}
+
+# Store the BiocGenerics %in% to differentiate from base::`%in%`
+`%gin%` <- function(a, b) {
+  BiocGenerics::match(a, b, nomatch = 0L) > 0L
+}
+
+#' Print `CompartmentCall`
+method(print, CompartmentCall) <- function(x, ...) {
+  message(.print_CompartmentCall(x))
+}
+
+.print_CompartmentCall <- function(x) {
+  properties <- props(x)
+  class_type <- gsub("compartmap::", "", class(x)[1])
+
+  sprintf(
+    "<%s object>
+  @name        : %s
+  @res         : %s
+  @gr          : GRanges with %d bins
+  @df          : data.table of compartment calls (n = bin index, pc = singular values)
+  @unitarized  : %s
+  @filtered    : %s%s",
+    class_type,
+    x@name,
+    .resolution(x@res),
+    length(x@gr),
+    x@unitarized,
+    x@filtered,
+    ifelse(x@filtered, paste(", for absolute values >=", x@filter_threshold), "")
+  )
+}
+
+#' @rdname CompartmentCall
+#'
+#' @keywords CompartmentCall
+#' @export
+CompartmapCall <- new_class(
+  "CompartmapCall",
+  parent = CompartmentCall,
+  constructor = function(gr, res, name = NULL, unitarized = FALSE) {
+    df <- data.table(pc = gr$pc)[, `:=`(n = .I, name = name)][, .(n, pc, name)]
+    new_object(
+      S7_object(),
+      name = name %||% shQuote(substitute(gr), "cmd2"),
+      gr = granges(gr),
+      df = df,
+      res = res,
+      unitarized = unitarized,
+      filtered = FALSE,
+      filter_threshold = 0,
+      seqinfo = methods::selectMethod("seqinfo", "GRanges")(gr)
+    )
+  }
+)
+S4_register(CompartmapCall)
---
+++ R/MultiCompartmentCall.R
@@ -0,0 +1,317 @@
+#' @rdname CompartmentCall
+#'
+#' @param ccalls A list of `CompartmapCalls` to combine
+#' @param name An identifier for this set of compartment calls
+#' @param unitarize Whether to unitarize the singular values for each of the inputs calls
+#' @param filter Whether to filter the singular values
+#' @param filter_threshold The threshold at which to filter the singular values
+#'
+#' @importFrom data.table rbindlist dcast
+#' @keywords CompartmentCall
+#' @export
+MultiCompartmapCall <- new_class(
+  "MultiCompartmapCall",
+  parent = CompartmentCall,
+  properties = list(
+    colnames = class_character,
+    mat = new_S3_class(c("matrix", "array"))
+  ),
+  constructor = function(ccalls, name, unitarize = FALSE, filter = FALSE, filter_threshold = 0.02) {
+    all_same <- function(prop_get, err) {
+      unique_property <- unique(unlist(lapply(ccalls, prop_get)))
+      if (length(unique_property) != 1) {
+        stop(err)
+      }
+      unique_property
+    }
+    unitarize_all <- function(i) {
+      if (!is_unitarized(i)) {
+        return(unitarize(i))
+      }
+      i
+    }
+
+    unique_res <- all_same(resolution, "All resolutions must be the same")
+    unique_gr <- all_same(granges, "All GRanges must contain the same ranges")
+    all_filtered <- all_same(is_filtered, "All objects must be filtered")
+    all_ft <- all_same(get_filter_threshold, "All objects must be filtered to the same threshold")
+
+    all_unitarized <- tryCatch(
+      all_same(
+        is_unitarized,
+        "All calls must be either unitarized or non-unitarized - set 'unitarize = TRUE' to unitarize them"
+      ),
+      error = function(e) {
+        if (unitarize) {
+          FALSE
+        } else {
+          stop(e)
+        }
+      }
+    )
+
+    if (unitarize) {
+      ccalls <- lapply(ccalls, unitarize)
+      all_unitarized <- TRUE
+      all_ft <- filter_threshold
+    }
+
+    colorder <- sapply(ccalls, get_name)
+    df <- rbindlist(lapply(ccalls, function(i) {
+      DF(i)[, name := get_name(i)][, name := factor(name, levels = colorder)][]
+    }))
+    mat <- as.matrix(dcast(df, n ~ name, value.var = "pc")[, -1])
+
+    obj <- new_object(
+      S7_object(),
+      name = name,
+      gr = unique_gr[[1]],
+      df = df,
+      res = unique_res,
+      unitarized = all_unitarized,
+      filtered = all_filtered,
+      filter_threshold = all_ft,
+      seqinfo = methods::selectMethod('seqinfo', "GRanges")(unique_gr[[1]]),
+      colnames = unlist(lapply(ccalls, get_name)),
+      mat = mat
+    )
+
+    if (filter) {
+      obj <- filter(obj, threshold = filter_threshold)
+    }
+    obj
+  },
+  validator = function(self) {
+    if (length(self@colnames) != length(unique(self@colnames))) {
+      "All names must be unique"
+    }
+  }
+)
+S4_register(MultiCompartmapCall)
+
+# GETTERS ==================================================================={{{
+`nrow.compartmap::CompartmentCall` <- function(x) {
+  nrow(x@mat)
+}
+
+`ncol.compartmap::CompartmentCall` <- function(x) {
+  ncol(x@mat)
+}
+
+#' Get dimensions of `MultiCompartmapCall` matrix
+#' @concept s7getters
+#' @export
+method(dim, MultiCompartmapCall) <- function(x) {
+  dim(x@mat)
+}
+
+#' Get column names of `MultiCompartmapCall` matrix
+#' @concept s7getters
+#' @export
+method(names, MultiCompartmapCall) <- function(x) {
+  colnames(x@mat)
+}
+
+#' Get the data matrix from a `MultiCompartmapCall` or `scCompartmapCall`
+#'
+#' @param x A `MultiCompartmapCall` object
+#'
+#' @concept s7getters
+#' @export
+mat <- new_generic("mat", "x", function(x) {
+  S7_dispatch()
+})
+method(mat, MultiCompartmapCall) <- function(x) {
+  x@mat
+}
+
+# }}}
+
+# SUBSETTERS ==============================================================={{{
+
+#' Subset `MultiCompartmapCall` object
+#'
+#' @param x A `CompartmentCall` object
+#' @param i Rows indices to subset
+#' @param j Column indices or names to subset
+#'
+#' @concept s7ranges
+#' @export
+`[.compartmap::MultiCompartmapCall` <- function(x, i = NULL, j = NULL) {
+  i <- i %||% seq_len(nrow(x@mat))
+  j <- j %||% seq_len(ncol(x@mat))
+
+  if (is.logical(i)) {
+    i <- which(i)
+  } else if (!is.numeric(i)) {
+    stop("Unsupported type for subsetting columns")
+  }
+  stopifnot("i exceeds the row count of x" = i <= nrow(x@mat))
+
+  if (is.numeric(j)) {
+    if (length(j) > ncol(x@mat)) {
+      stop("j exceeds the column count of x")
+    }
+    subset_names <- x@df[, unique(name)][j]
+  } else if (is.character(j)) {
+    stopifnot("One or more columns not found in the data" = all(j %in% colnames(x@mat)))
+    subset_names <- j
+  } else {
+    stop("Unsupported type for subsetting columns")
+  }
+
+  x@gr <- x@gr[i]
+  seqlevels(x@gr) <- seqlevelsInUse(x@gr)
+  x@mat <- x@mat[i, j, drop = FALSE]
+  x@df <- x@df[n %in% i & name %in% subset_names]
+  x@df[, n := seq_len(.N), by = name][]
+  x
+}
+
+# }}}
+
+#' Print a `CompartmentCall` object
+#'
+#' @param x A `CompartmentCall` object
+#'
+#' @export
+method(print, MultiCompartmapCall) <- function(x, ...) {
+  column_label <- ifelse(inherits(x, "compartmap::scCompartmapCall"), "cells", "samples")
+  msg <- message(
+    .print_CompartmentCall(x),
+    sprintf("\n  @mat         : %d bins x %d %s", nrow(x@mat), ncol(x@mat), column_label)
+  )
+}
+
+#' @concept s7analysis
+#' @export
+method(filter, MultiCompartmapCall) <- function(x, threshold = 0.02) {
+  filter_rows <- apply(x@mat, 1, function(i) {
+    all(abs(i) >= threshold)
+  })
+  x <- x[filter_rows]
+  x@filtered <- TRUE
+  x@filter_threshold <- threshold
+  x
+}
+
+#' Compute agreement between compartment calls
+#'
+#' Calculates the proportion of calls with the same sign for every pair of
+#' calls in a `MultiCompartmapCall` object.
+#'
+#' @param x A `MultiCompartmapCall` object
+#' @param na.omit Whether to omit NAs
+#'
+#' @concept s7analysis
+#' @export
+agreement <- new_generic("agreement", "x", function(x, na.omit = TRUE) {
+  S7_dispatch()
+})
+method(agreement, MultiCompartmapCall) <- function(x, na.omit = TRUE) {
+  if (na.omit) {
+    get_agreement(na.omit(x@mat))
+  } else {
+    get_agreement(x@mat)
+  }
+}
+
+#' Compute correlation between compartment calls
+#'
+#' Calculates Pearson correlation for every pair of calls in a
+#' MultiCompartmapCall object.
+#'
+#' @param x A `MultiCompartmapCall` object
+#' @param na.omit Whether to omit NAs
+#'
+#' @importFrom stats cor
+#' @concept s7analysis
+#' @export
+corr <- new_generic("corr", "x", function(x, na.omit = TRUE) {
+  S7_dispatch()
+})
+method(corr, MultiCompartmapCall) <- function(x, na.omit = TRUE) {
+  if (na.omit) {
+    cor(na.omit(x@mat))
+  } else {
+    cor(x@mat)
+  }
+}
+
+#' Compute the direction of change of singular vectors in the MultiCompartmentCall object
+#'
+#' The direction of change is computed by chromosome
+#'
+#' @param x A `MultiCompartmentCall` object
+#' @value A `MultiCompartmentCall` object with the gradients in the `@df` slot,
+#' replacing the singular vectors
+#'
+#' @concept s7analysis
+#' @export
+differentiate <- new_generic("differentiate", "x", function(x) {
+  S7_dispatch()
+})
+method(differentiate, MultiCompartmapCall) <- function(x) {
+  x@df <- x@df[, .(n, pc, name, chr = seqlevels(x))] |>
+    _[, .(pc = c(NA, diff(pc[-1])) / diff(n)), by = .(name, chr)] |>
+    _[, n := seq_len(.N), by = "name"] |>
+    na.omit()
+  x@mat <- as.matrix(dcast(x@df, n ~ name, value.var = "pc")[, -1])
+  x
+}
+
+#' Plot singular values from a `MultiCompartmapCall` object
+#'
+#' @param x `MultiCompartmapCall`
+#' @param ... Placeholder for the `plot` generic - arguments have not effect
+#' @param type Whether to plot the singular values as `"line"`plots. Bar plots
+#' will be facted by the individual `CompartmapCall` object names while the
+#' line plots are overlayed.
+#' @param label_coords Label the x-axis with genomic coordinates. Uses a
+#' numeric index when set to `FALSE`. Using coordinate labels can severely
+#' crowd the x-axis, especially with Kb-resolution calls.
+#' @param res The resolution to round the genomic coordinates to (kilobase:
+#' "kb" or megabase: "mb")
+#' @param width The width of the `geom_line` if `type = "line"` or the width
+#' of the bar if `type = "bar"` in the plot
+#' @param ylim Upper and lower bound for the y-axis
+#'
+#' @importFrom ggplot2 ggplot geom_line geom_col scale_y_continuous theme element_text facet_grid vars scale_fill_manual
+#' @concept plotting
+#' @export
+`plot.compartmap::MultiCompartmapCall` <- function(
+  x,
+  ...,
+  type = "line",
+  label_coords = FALSE,
+  res = "mb",
+  width = 1.0,
+  ylim = c(-1, 1)
+) {
+  pd <- x@df
+  x_axis <- "n"
+  if (label_coords) {
+    pd <- x@df[, .(n, pc, name, coord = grscale(x@gr, res))]
+    x_axis <- "coord"
+  }
+
+  p <- switch(
+    type,
+    line = {
+      ggplot(pd, aes(x = .data[[x_axis]], y = pc, color = name, group = name)) +
+        geom_line()
+    },
+    bar = {
+      ggplot(pd, aes(x = .data[[x_axis]], y = pc, group = name, fill = pc > 0)) +
+        geom_col(width = width) +
+        facet_grid(rows = vars(name)) +
+        scale_fill_manual(values = c("deeppink4", "grey50")) +
+        theme(legend.position = "none")
+    }
+  )
+
+  if (label_coords) {
+    p <- theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+  }
+  p + scale_y_continuous(limits = ylim)
+}

b48561bd378b052b1f2e0f9d09a69032f0be7197
James Eapen <james.eapen@vai.org>
2026-03-24 15:32:18 -0400

    fix(S7): allow multiple assays in a MultiCompartmapCall

--- R/MultiCompartmapCall.R
+++ R/MultiCompartmapCall.R
@@ -32,10 +32,11 @@ MultiCompartmapCall <- new_class(
     }
 
     unique_res <- all_same(resolution, "All resolutions must be the same")
-    unique_assay <- all_same(get_assay, "All assays must be the same")
     unique_gr <- all_same(granges, "All GRanges must contain the same ranges")
     all_filtered <- all_same(is_filtered, "All objects must be filtered")
     all_ft <- all_same(get_filter_threshold, "All objects must be filtered to the same threshold")
+
+    unique_assays <- unique(sapply(ccalls, get_assay))
 
     all_unitarized <- tryCatch(
       all_same(
@@ -69,7 +70,7 @@ MultiCompartmapCall <- new_class(
       gr = unique_gr[[1]],
       df = df,
       res = unique_res,
-      assay = unique_assay,
+      assay = paste(unique_assays, collapse = ", "),
       unitarized = all_unitarized,
       filtered = all_filtered,
       filter_threshold = all_ft,

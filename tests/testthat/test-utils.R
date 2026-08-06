0088cae11a27f2a986cf4455e79b594e47ab68f5
James Eapen <james.eapen@vai.org>
2026-08-06 14:10:59 -0400

    test(utils): remove extractOpenClosed

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -11,65 +11,6 @@ gr <- GRanges(
   IRanges(1:10, width = 10:1)
 )
 
-# extractOpenClosed {{{
-gr.pc <- gr
-pc.list <- list('pc' = c(1, -1, 1, -1, -2, 2, -5, 5, -10, 10))
-pc.state.cutoff.zero <- c(
-  "open",
-  "closed",
-  "open",
-  "closed",
-  "closed",
-  "open",
-  "closed",
-  "open",
-  "closed",
-  "open"
-)
-pc.state.cutoff.two <- c(
-  "closed",
-  "closed",
-  "closed",
-  "closed",
-  "closed",
-  "closed",
-  "closed",
-  "open",
-  "closed",
-  "open"
-)
-pc.state.array <- lapply(
-  pc.state.cutoff.zero,
-  function(i) ifelse(i == "open", "closed", "open")
-) |>
-  unlist()
-mcols(gr.pc) <- pc.list
-
-test_that("extractOpenClosed", {
-  expect_error(extractOpenClosed(se))
-  expect_error(extractOpenClosed(re))
-  expect_error(extractOpenClosed(df))
-  expect_error(extractOpenClosed(gr))
-  expect_no_error(extractOpenClosed(gr.pc))
-  expect_equal(
-    extractOpenClosed(gr.pc, assay = "rna"),
-    pc.state.cutoff.zero
-  )
-  expect_equal(
-    extractOpenClosed(gr.pc, assay = "rna", cutoff = 2),
-    pc.state.cutoff.two
-  )
-  expect_equal(
-    extractOpenClosed(gr.pc, assay = "atac"),
-    pc.state.cutoff.zero
-  )
-  expect_equal(
-    extractOpenClosed(gr.pc, assay = "array"),
-    pc.state.array
-  )
-})
-# }}}
-
 # verifySE{{{
 test_that("verifySE", {
   expect_no_error(compartmap:::verifySE(se))
